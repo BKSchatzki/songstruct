c0e1d245a90a40603a97d0f81c648ab08080d6e3
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-07-17 23:37:19 -0700

    Add margin and shading to body to relieve eye strain

--- src/components/TestContainer.jsx
+++ src/components/TestContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function TestContainer({ children }) {
   return (
-    <div className="border-4 border-slate-800 bg-slate-200">{children}</div>
+    <div className="m-4 border-4 border-slate-800 bg-slate-200">{children}</div>
   );
 }
 
