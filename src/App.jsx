daa1f623eb04ec104a492940afd135e44cc95e56
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-07-18 13:57:06 -0700

    Create components for new architecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import "./index.css";
-import CheckboxGrid from "./components/CheckboxGrid";
 import TestContainer from "./components/TestContainer";
+import CheckboxGrid from "./components/CheckboxGrid";
+import DivGrid from "./components/DivGrid";
+import Cell from "./components/Cell";
+import Header from "./components/Header";
+import Row from "./components/Row";
 
 function App() {
   return (
     <div className="h-screen bg-slate-300">
       <TestContainer>
         <CheckboxGrid />
+      </TestContainer>
+      <TestContainer>
+        <DivGrid />
+      </TestContainer>
+      <TestContainer>
+        <Cell />
+      </TestContainer>
+      <TestContainer>
+        <Header />
+      </TestContainer>
+      <TestContainer>
+        <Row />
       </TestContainer>
     </div>
   );
---
+++ src/components/DivGrid.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DivGrid() {
+  return (
+    <div>
+      <h1>Hi</h1>
+    </div>
+  );
+}
+
+export default DivGrid;
