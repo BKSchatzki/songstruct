b076b22b06a0cb3c950e249302bfafd9a0decae6
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-07-18 00:23:16 -0700

    Add basic styling to row and column add and delete buttons

--- src/components/CheckboxGrid.jsx
+++ src/components/CheckboxGrid.jsx
@@ -1,21 +1,61 @@
 import React, { useState } from "react";
+import { PlusSquare, MinusSquare } from "react-feather";
 
 const CheckboxGrid = () => {
   const [rows, setRows] = useState(2);
   const [columns, setColumns] = useState(2);
-  const [rowLabels, setRowLabels] = useState(["Row 1", "Row 2"]);
+  const [rowLabels, setRowLabels] = useState(["", ""]);
 
   const handleAddRow = () => {
     setRows(rows + 1);
-    setRowLabels([...rowLabels, `Row ${rows + 1}`]);
+    setRowLabels([...rowLabels, ""]);
+  };
+
+  const handleDeleteRow = () => {
+    if (rows > 1) {
+      setRows(rows - 1);
+      setRowLabels(rowLabels.slice(0, -1));
+    }
   };
 
   const handleAddColumn = () => {
     setColumns(columns + 1);
   };
 
+  const handleDeleteColumn = () => {
+    if (columns > 1) {
+      setColumns(columns - 1);
+    }
+  };
+
+  const handleRowLabelChange = (event, rowIndex) => {
+    const updatedRowLabels = [...rowLabels];
+    updatedRowLabels[rowIndex] = event.target.value;
+    setRowLabels(updatedRowLabels);
+  };
+
   return (
     <div>
+      <div className="flex flex-row pb-4">
+        <div className="mx-2 flex items-center text-xl uppercase">
+          <button onClick={handleDeleteRow}>
+            <MinusSquare />
+          </button>
+          <span className="mx-1">Row</span>
+          <button onClick={handleAddRow}>
+            <PlusSquare />
+          </button>
+        </div>
+        <div className="mx-2 flex items-center text-xl uppercase">
+          <button onClick={handleDeleteColumn}>
+            <MinusSquare />
+          </button>
+          <span className="mx-1">Col</span>
+          <button onClick={handleAddColumn}>
+            <PlusSquare />
+          </button>
+        </div>
+      </div>
       <table>
         <thead>
           <tr>
@@ -23,15 +63,19 @@ const CheckboxGrid = () => {
             {[...Array(columns)].map((_, index) => (
               <th key={index}></th>
             ))}
-            <th>
-              <button onClick={handleAddColumn}>+</button>
-            </th>
           </tr>
         </thead>
         <tbody>
           {rowLabels.map((label, rowIndex) => (
             <tr key={rowIndex}>
-              <th>{label}</th>
+              <th>
+                <input
+                  type="text"
+                  placeholder={`Instrument ${rowIndex + 1}`}
+                  value={label}
+                  onChange={(event) => handleRowLabelChange(event, rowIndex)}
+                />
+              </th>
               {[...Array(columns)].map((_, colIndex) => (
                 <td key={colIndex}>
                   <input type="checkbox" />
@@ -39,15 +83,6 @@ const CheckboxGrid = () => {
               ))}
             </tr>
           ))}
-          <tr>
-            <th></th>
-            {[...Array(columns)].map((_, index) => (
-              <th key={index}></th>
-            ))}
-            <th>
-              <button onClick={handleAddRow}>+</button>
-            </th>
-          </tr>
         </tbody>
       </table>
     </div>
