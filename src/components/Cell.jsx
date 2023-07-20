9ef436887106a618f5319a7587f056ecd561aabe
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-07-19 23:53:11 -0700

    Rename state handlers in Cell

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -2,23 +2,21 @@ import React, { useState } from "react";
 
 function Cell(props) {
   const { colors } = props;
-  const [opacityClasses, setOpacityClasses] = useState([
+  const [cellState, setCellState] = useState([
     `opacity-10 bg-neutral-content`,
-    `opacity-50 bg${colors}`,
+    `opacity-40 bg${colors}`,
     `opacity-100 bg${colors}`,
   ]);
-  const [currentOpacityIndex, setCurrentOpacityIndex] = useState(0);
+  const [currentCellStateIndex, setCurrentCellStateIndex] = useState(0);
 
   const handleCellClick = () => {
     // Cycle through the opacity classes
-    setCurrentOpacityIndex(
-      (prevIndex) => (prevIndex + 1) % opacityClasses.length,
-    );
+    setCurrentCellStateIndex((prevIndex) => (prevIndex + 1) % cellState.length);
   };
 
   return (
     <div
-      className={`ms-4 h-12 w-12 rounded-lg transition duration-100 ease-in-out bg${colors} border${colors} ${opacityClasses[currentOpacityIndex]}`}
+      className={`ms-4 h-12 w-12 rounded-lg transition duration-100 ease-linear ${cellState[currentCellStateIndex]}`}
       onClick={handleCellClick}
     ></div>
   );
