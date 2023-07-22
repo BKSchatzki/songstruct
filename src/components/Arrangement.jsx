b2ebdb0b14a299f5e5968d90914c54a92c536faf
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-07-22 00:15:58 -0700

    Use Daisy colors and lower lanes from 9 to 7

--- src/components/Arrangement.jsx
+++ src/components/Arrangement.jsx
@@ -4,15 +4,22 @@ import Section from "./Section";
 
 function Arrangement() {
   const colors = [
-    "-red-500",
-    "-amber-500",
-    "-lime-500",
-    "-emerald-500",
-    "-cyan-500",
-    "-blue-500",
-    "-violet-500",
-    "-fuchsia-500",
-    "-rose-500",
+    "-primary",
+    "-secondary",
+    "-accent",
+    "-info",
+    "-success",
+    "-warning",
+    "-error",
+    // "-red-500",
+    // "-amber-500",
+    // "-lime-500",
+    // "-emerald-500",
+    // "-cyan-500",
+    // "-blue-500",
+    // "-violet-500",
+    // "-fuchsia-500",
+    // "-rose-500",
   ];
 
   // State to keep track of sections
--- src/utilities/Dummy.jsx
+++ src/utilities/Dummy.jsx
@@ -4,6 +4,13 @@ function Dummy() {
   return (
     <div>
       {/* Background Colors */}
+      <div className="bg-primary" />
+      <div className="bg-secondary" />
+      <div className="bg-accent" />
+      <div className="bg-info" />
+      <div className="bg-success" />
+      <div className="bg-warning" />
+      <div className="bg-error" />
       <div className="bg-red-500" />
       <div className="bg-amber-500" />
       <div className="bg-lime-500" />
@@ -14,6 +21,13 @@ function Dummy() {
       <div className="bg-fuchsia-500" />
       <div className="bg-rose-500" />
       {/* Background Colors Focus*/}
+      <div className="hover:bg-primary" />
+      <div className="hover:bg-secondary" />
+      <div className="hover:bg-accent" />
+      <div className="hover:bg-info" />
+      <div className="hover:bg-success" />
+      <div className="hover:bg-warning" />
+      <div className="hover:bg-error" />
       <div className="hover:bg-red-500" />
       <div className="hover:bg-amber-500" />
       <div className="hover:bg-lime-500" />
@@ -24,6 +38,13 @@ function Dummy() {
       <div className="hover:bg-fuchsia-500" />
       <div className="hover:bg-rose-500" />
       {/* Border Colors */}
+      <div className="border-primary" />
+      <div className="border-secondary" />
+      <div className="border-accent" />
+      <div className="border-info" />
+      <div className="border-success" />
+      <div className="border-warning" />
+      <div className="border-error" />
       <div className="border-red-500" />
       <div className="border-amber-500" />
       <div className="border-lime-500" />
@@ -34,6 +55,13 @@ function Dummy() {
       <div className="border-fuchsia-500" />
       <div className="border-rose-500" />
       {/* Text Colors */}
+      <div className="text-primary" />
+      <div className="text-secondary" />
+      <div className="text-accent" />
+      <div className="text-info" />
+      <div className="text-success" />
+      <div className="text-warning" />
+      <div className="text-error" />
       <div className="text-red-500" />
       <div className="text-amber-500" />
       <div className="text-lime-500" />
@@ -44,6 +72,13 @@ function Dummy() {
       <div className="text-fuchsia-500" />
       <div className="text-rose-500" />
       {/* Text Colors Placeholders */}
+      <div className="placeholder:text-primary" />
+      <div className="placeholder:text-secondary" />
+      <div className="placeholder:text-accent" />
+      <div className="placeholder:text-info" />
+      <div className="placeholder:text-success" />
+      <div className="placeholder:text-warning" />
+      <div className="placeholder:text-error" />
       <div className="placeholder:text-red-500" />
       <div className="placeholder:text-amber-500" />
       <div className="placeholder:text-lime-500" />
