d082ff5e6717ed773c3da4076d0e1d92aa48a3ea
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-07-21 23:35:47 -0700

    Reduce horizontal padding on Section

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -6,7 +6,7 @@ function Section(props) {
   const { colors, onAddSection, onDeleteSection } = props;
 
   return (
-    <div className="mt-2 flex flex-col gap-1 rounded-2xl px-4 pb-4 shadow-2xl">
+    <div className="mt-2 flex flex-col gap-1 rounded-2xl px-3 pb-4 shadow-2xl">
       <Divider onAddSection={onAddSection} onDeleteSection={onDeleteSection} />
       <div className="flex flex-row items-start justify-center gap-2">
         {colors.map((color, index) => (
